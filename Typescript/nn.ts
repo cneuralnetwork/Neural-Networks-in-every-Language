03e60829c761f9c0dee35c05f3f5f402851a7549
mdnihal5 <mdnihal.private1@gmail.com>
2024-10-02 13:44:57 +0530

    added typescript --fixed typos

--- Typescript/nn.ts
+++ Typescript/nn.ts
@@ -1,10 +1,10 @@
 import * as tf from '@tensorflow/tfjs';
 
 function initParams() {
-  nst w1 = tf.randomUniform([10, 784], -0.5, 0.5);
-  nst b1 = tf.randomUniform([10, 1], -0.5, 0.5);
-  nst w2 = tf.randomUniform([10, 10], -0.5, 0.5);
-  nst b2 = tf.randomUniform([10, 1], -0.5, 0.5);
+  const w1 = tf.randomUniform([10, 784], -0.5, 0.5);
+  const b1 = tf.randomUniform([10, 1], -0.5, 0.5);
+  const w2 = tf.randomUniform([10, 10], -0.5, 0.5);
+  const b2 = tf.randomUniform([10, 1], -0.5, 0.5);
   return { w1, b1, w2, b2 };
 }
 
@@ -21,21 +21,21 @@ function oneHot(y: number[], numClasses: number): tf.Tensor {
 }
 
 function forwardProp(w1: tf.Tensor, b1: tf.Tensor, w2: tf.Tensor, b2: tf.Tensor, x: tf.Tensor) {
-  nst z1 = w1.matMul(x).add(b1);
-  nst a1 = ReLU(z1);
-  nst z2 = w2.matMul(a1).add(b2);
-  nst a2 = softmax(z2);
+  const z1 = w1.matMul(x).add(b1);
+  const a1 = ReLU(z1);
+  const z2 = w2.matMul(a1).add(b2);
+  const a2 = softmax(z2);
   return { z1, a1, z2, a2 };
 }
 
 function backProp(z1: tf.Tensor, a1: tf.Tensor, a2: tf.Tensor, w2: tf.Tensor, x: tf.Tensor, y: tf.Tensor) {
-  nst m = y.shape[1];
-  nst dz2 = a2.sub(y);
-  nst dw2 = dz2.matMul(a1.transpose()).div(m);
-  nst db2 = dz2.sum(1).reshape([10, 1]).div(m);
-  nst dz1 = w2.transpose().matMul(dz2).mul(z1.greater(0));
-  nst dw1 = dz1.matMul(x.transpose()).div(m);
-  nst db1 = dz1.sum(1).reshape([10, 1]).div(m);
+  const m = y.shape[1];
+  const dz2 = a2.sub(y);
+  const dw2 = dz2.matMul(a1.transpose()).div(m);
+  const db2 = dz2.sum(1).reshape([10, 1]).div(m);
+  const dz1 = w2.transpose().matMul(dz2).mul(z1.greater(0));
+  const dw1 = dz1.matMul(x.transpose()).div(m);
+  const db1 = dz1.sum(1).reshape([10, 1]).div(m);
   return { dw1, db1, dw2, db2 };
 }
 
@@ -73,17 +73,17 @@ async function gradientDescent(X: tf.Tensor, Y: tf.Tensor, iterations: number, a
 }
 
 function makePredictions(X: tf.Tensor, w1: tf.Tensor, b1: tf.Tensor, w2: tf.Tensor, b2: tf.Tensor) {
-  nst { a2 } = forwardProp(w1, b1, w2, b2, X);
+  const { a2 } = forwardProp(w1, b1, w2, b2, X);
   return getPredictions(a2);
 }
 
 async function main() {
-  nst X_train = tf.randomUniform([784, 1000]);
-  nst Y_train = oneHot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10).tile([1, 100]);
+  const X_train = tf.randomUniform([784, 1000]);
+  const Y_train = oneHot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10).tile([1, 100]);
 
-  nst { w1, b1, w2, b2 } = await gradientDescent(X_train, Y_train, 500, 0.1);
-  nst X_test = tf.randomUniform([784, 10]);  // 10 test examples
-  nst predictions = makePredictions(X_test, w1, b1, w2, b2);
+  const { w1, b1, w2, b2 } = await gradientDescent(X_train, Y_train, 500, 0.1);
+  const X_test = tf.randomUniform([784, 10]);  // 10 test examples
+  const predictions = makePredictions(X_test, w1, b1, w2, b2);
   edictions.print();
 }
 
@@ -94,15 +94,15 @@ main();
 /*import * as tf from '@tensorflow/tfjs';
 
 async function testNN() {
-    const w1 = tf.randomUniform([10, 784], -0.5, 0.5);
-    const b1 = tf.randomUniform([10, 1], -0.5, 0.5);
-    const w2 = tf.randomUniform([10, 10], -0.5, 0.5);
-    const b2 = tf.randomUniform([10, 1], -0.5, 0.5);
+    coconst w1 = tf.randomUniform([10, 784], -0.5, 0.5);
+    coconst b1 = tf.randomUniform([10, 1], -0.5, 0.5);
+    coconst w2 = tf.randomUniform([10, 10], -0.5, 0.5);
+    coconst b2 = tf.randomUniform([10, 1], -0.5, 0.5);
 
-    const X_train = tf.randomUniform([784, 1000]);
-    const Y_train = tf.oneHot(tf.tensor1d([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'int32'), 10).tile([100, 1]);
+    coconst X_train = tf.randomUniform([784, 1000]);
+    coconst Y_train = tf.oneHot(tf.tensor1d([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'int32'), 10).tile([100, 1]);
 
-    const { z1, a1, z2, a2 } = forwardProp(w1, b1, w2, b2, X_train);
+    coconst { z1, a1, z2, a2 } = forwardProp(w1, b1, w2, b2, X_train);
     console.log("Forward Propagation successful");
 }
 
